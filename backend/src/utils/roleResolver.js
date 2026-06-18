86e30d6f7d02955eff02e0ca538f864a1782d623
Trn Trung Hiu <trantrunghieu.contact@gmail.com>
2026-06-18 12:25:40 +0700

    fix: hardcode khiemlt@mindx.com.vn to have TE role

--- backend/src/utils/roleResolver.js
+++ backend/src/utils/roleResolver.js
@@ -72,7 +72,9 @@ function resolveUserRolesAndProfile(baseUser, roleInfos = []) {
   const isKheim =
     baseUser.username === "lekhiem2002" ||
     baseUser.email === "lekhiem2002@mindx.net.vn" ||
-    baseUser.email === "lethekhiem2002@mindx.net.vn";
+    baseUser.email === "lethekhiem2002@mindx.net.vn" ||
+    baseUser.username === "I3470" ||
+    baseUser.email === "khiemlt@mindx.com.vn";
 
   if (isKheim) {
     appRoles.add(ROLES.TE);
